db98313ddfb6be3246d246f0dc385508bbf061b7
Adam Wieliczko <niszczyciel.90@wp.pl>
2022-02-26 11:20:18 +0100

    Push WebCrawler project

---
+++ WebCrawler/Crawler.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace WebCrawler
+{
+    internal class Crawler
+    {
+        public static int finalDepth;
+        public static List<string> finalPath;
+        public static String target = "http://en.wikipedia.org/wiki/Elizabeth_II";
+        public static ConcurrentQueue<(string, int, List<string>)> queue = new(); //ostatni argument czyli lista stringów to lista przez jakie lokalizacje przechodzł Crawler
+
+        public void startCrawler()
+        {
+            while (finalDepth == -1)
+            {
+                (string, int, List<string>) currentPair;
+                if(!queue.TryDequeue(out currentPair))
+                {
+                    continue;
+                }
+
+                if(currentPair.Item1.Equals(target))
+                {
+                    finalPath = currentPair.Item3;
+                    finalDepth = currentPair.Item2;
+                    return;
+                }
+
+                WebClient web = new WebClient();
+                String text = "";
+                System.IO.Stream stream = web.OpenRead(currentPair.Item1);
+
+                using (System.IO.StreamReader reader = new System.IO.StreamReader(stream))
+                {
+                    text = reader.ReadToEnd();
+                }
+
+                List<String> hrefList = new List<String>();
+
+                Regex.Matches(text, "<a href=\"/wiki/([^\":()]*)\"", RegexOptions.Compiled)
+                    .Cast<Match>().Select(i => i.Value).ToList().ForEach(i => hrefList.Add("http://en.wikipedia.org" + i.Substring(9, i.Length - 10)));
+
+                //
+
+                if(hrefList.Any(i => i.Equals(target)))
+                {
+                    var newList = new List<String>(currentPair.Item3);
+                    newList.Add(currentPair.Item1);
+                    finalPath = newList;
+                    finalDepth = currentPair.Item2 + 1;
+                    return;
+                }
+
+                foreach (var href in hrefList)
+                {
+                    var newList = new List<String>(currentPair.Item3);
+                    newList.Add(currentPair.Item1);
+                    queue.Enqueue((href, currentPair.Item2 + 1, newList));
+                }
+            }
+        }
+    }
+}
---
+++ WebCrawler/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCrawler
+{
+    internal class Program
+    {
+
+        public static void Main(String[] args)
+        {
+            Crawler.queue.Enqueue(("http://en.wikipedia.org/wiki/Special:Random", 0, new List<string>()));
+            Crawler.finalDepth = -1;
+            Crawler.finalPath = new List<string>();
+            var crawler = new Crawler();
+            Stopwatch k = new Stopwatch();
+            int howManyTasks = 10;
+            k.Start();
+            Task[] tasks = new Task[howManyTasks];
+            for(int i = 0; i < howManyTasks; i++)
+            {
+                tasks[i] = new Task(() => crawler.startCrawler());
+                tasks[i].Start();
+            }
+
+            Task.WaitAll(tasks);
+
+            if(Crawler.finalDepth >= 0)
+            {
+                k.Stop();
+                Console.WriteLine("Found target in " + Crawler.finalDepth + " time " + k.ElapsedMilliseconds);
+                foreach(var dir in Crawler.finalPath)
+                {
+                    Console.Write(dir + " -> ");
+                }
+                Console.Write(Crawler.target);
+            }
+            else
+            {
+                Console.WriteLine("Can't find connection to target");
+            }
+        }
+    }
+}
